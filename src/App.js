331e12313a6dfeb0e61de531353a0b0cdd5214c9
m-uson <usonibrahimovic@gmai.com>
2022-05-19 23:26:40 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './style.css'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="title">Привет Чунбурунчупмеки!</h1>
+    </div>
+  );
+}
